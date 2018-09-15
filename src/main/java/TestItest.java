82e9e5c6f1cdebfed23ba3595d361594ad1da7ed
Rus <r.1997@yandex.ru>
2018-09-15 20:39:05 +0300

    Определенные буквы вверх+описание

--- src/main/java/TestItest.java
+++ src/main/java/TestItest.java
@@ -3,25 +3,27 @@ import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.colors.DeviceCmyk;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.ElementPropertyContainer;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
 import com.itextpdf.layout.hyphenation.HyphenationConfig;
-import com.itextpdf.layout.splitting.ISplitCharacters;
-import org.apache.commons.lang.RandomStringUtils;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
 
 import java.io.File;
 import java.util.ArrayList;
 
 
 class TestItest {
-    ArrayList<String>  WordList = new ArrayList<>();
-    String MainWord = "При жизни Серова картина экспонировалась на Таврической выставке (1905), организованной Сергеем Дягилевым, на Русской художественной выставке в Париже (1906). Кроме того, портрет демонстрировался на коллективных выставках Союза русских художников. В 1920 году картина перешла в собственность Малого театра. С 1935 года находится в Государственной Третьяковской галерее (инв. номер 28079). Там же хранится единственный известный искусствоведам эскиз к портрету, выполненный Серовым и показывающий, что ещё до начала непосредственной работы художник определил композицию, выбрал ракурс, придающий образу монументальность, и решил сделать акцент на силуэте актрисы.";
-    public static final String DEST = "./fonts/tutorial/f05_russian_encoding.pdf";
+
+   private String MainWord = "При жизни Серова картина экспонировалась на Таврической выставке (1905), организованной Сергеем Дягилевым, на Русской художественной выставке в Париже (1906). Кроме того, портрет демонстрировался на коллективных выставках Союза русских художников. В 1920 году картина перешла в собственность Малого театра. С 1935 года находится в Государственной Третьяковской галерее (инв. номер 28079). Там же хранится единственный известный искусствоведам эскиз к портрету, выполненный Серовым и показывающий, что ещё до начала непосредственной работы художник определил композицию, выбрал ракурс, придающий образу монументальность, и решил сделать акцент на силуэте актрисы.";
+    public static final String DEST = "./fonts/tutorial/test.pdf";
     public static final String FONT = "D:\\birch111.ttf";
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
@@ -34,92 +36,24 @@ class TestItest {
 
         //ArrayList<String> arrayList = new ArrayList<>();
         //arrayList.add("xxxxx");
-
-        String b1 = "Привет ебаный мир(тестовая версия)";//
+       String b1 = "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii1iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";//
         Text text1 = new Text(b1);//
-
+        PageSize ps = PageSize.A4;
         text1.setFont(font).setFontSize(15);//
-        String s = "fdfd";
         Paragraph p1 = new Paragraph(text1);//
-       // p1.add(text1);
-       // p1.add(text1);
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
-        Document doc = new Document(pdfDoc);
+        Document doc = new Document(pdfDoc,ps);
+        doc.setLeftMargin(0);
+        doc.setRightMargin(0);
+        float pdfWidth1 =  pdfDoc.getDefaultPageSize().getWidth();
+        float pdfWidth = pdfDoc.getDefaultPageSize().getWidth()-(doc.getRightMargin()+doc.getLeftMargin());
+        float getWithFont = font.getWidth(b1,15 );
+        float width1 = getWithFont;
+        width1 /= 1000;
+        System.out.println("a4 "  + pdfWidth1);
+        System.out.println("width font" +getWithFont);
+       doc.add(p1);//
 
-
-        Paragraph p = new Paragraph();
-        String sa = "Приветит2f";
-        String sub = "";
-
-        int j = 1;
-        int m = 0;
-        float b = (float) 0.3;
-        int afValue = sa.indexOf('в');
-        for (int i = 0; i < sa.length(); i++) {
-            Text t = new Text("");
-
-
-
-              sub = sa.substring(afValue+m,afValue+j);
-              if (sa.charAt(i) == 'в') {
-                  t.setText(String.valueOf(sa.charAt(i)));
-
-                  //t2.setTextRise((float) 1.2);
-
-                  //t2.setSkew(0,10);
-                  t.setRelativePosition(0, 0, 0, 1f);
-//                  subcount = +1;
-                  j++;
-                  m++;
-
-
-              } else if (sa.charAt(i) == sub.charAt(0)) {
-                  t.setText(String.valueOf(sa.charAt(i)));
-                  //t2.setTextRise((float) 1.2);
-                  //t2.setSkew(0,10);
-
-                  t.setRelativePosition(0, 0, 0, (float) (1+b));
-
-                  j++;
-                  m++;
-                  b+=0.6;
-                  System.out.println("BBB" +b);
-
-          }
-
-            t.setText(String.valueOf(sa.charAt(i)));
-
-            p.add(t);
-
-
-        }
-
-//
-//
-//
-//
-//
-//        Paragraph p2 = new Paragraph();
-//
-//        ArrayList<String> stringArrayList = new ArrayList<>();
-//stringArrayList.add("Привет");
-//stringArrayList.add("мир");
-//
-//        for (int i = 0; i <stringArrayList.size(); i++)
-//        {
-//
-//            System.out.println(stringArrayList.get(i));
-//            for(int j = 0; j<stringArrayList.get(i).length();j++) {
-//                Text t2 = new Text("");
-//                if(stringArrayList.get(i).charAt(j) == 'и') {
-//                    t2.setText(String.valueOf(stringArrayList.get(i).charAt(j)));
-//                    System.out.println(String.valueOf(stringArrayList.get(i).charAt(j)));
-//                    t2.setTextRise(5);
-//                }
-//                t2.setText(String.valueOf(stringArrayList.get(i).charAt(j)));
-//                p2.add(t2);
-//            }
-//        }
 
         PdfCanvas canvas = new PdfCanvas(pdfDoc.addNewPage());
         Color magentaColor = new DeviceCmyk(0.f, 1.f, 0.f, 0.f);
@@ -135,8 +69,7 @@ class TestItest {
         canvas.closePathStroke();
 
 
-      doc.add(returnRise().setFont(font));//
-        //doc.add(returnRise().setFont(font).setFixedLeading(20));
+       // doc.add(returnRise().setFont(font).setFixedLeading(20).setHyphenation(new HyphenationConfig("ru", "none", 2, 2)));
         doc.close();
     }
 
@@ -158,8 +91,6 @@ class TestItest {
                 float countSetRise = (float) 0.6; //Устанавливает setRise и увиеличивается каждый раз
                 int firssub = 0;// substring(+ firssub, + secondsub
                 int secondsub = 1; //
-
-
 
                 char c = w.RandomLetter(stringArrayList.get(i));//Ранлом буква с которой будет начинаться setRise
                 char s = w.RandomLetter(stringArrayList.get(i));
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,40 +1,91 @@
-import org.apache.commons.lang.RandomStringUtils;
 
+
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.layer.PdfLayer;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.ColumnDocumentRenderer;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.VerticalAlignment;
+
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.List;
+
 
 public class test {
-    public static void main(String[] args) {
-//        Random r = new Random();
-//        r.ints(20);
-//        String alphabet = "абмвгдезжиклмнопрстуфхцчщ";
-//        for (int i = 0; i < 50; i++) {
-//
-//            //System.out.println(alphabet.charAt(r.nextInt(10)));
-//        } // prints 50 random characters from alphabet
-//        String a = RandomStringUtils.random(alphabet.length()/5,alphabet);
-//       for(int i = 0; i<a.length();i++)
-//           System.out.println(a.charAt(i));
-//    }
-//        String chars = "Привет";
-//        Random rnd = new Random();
-//        char c = chars.charAt(rnd.nextInt(chars.length()));
-//        System.out.println(c);
-//        StringBuilder stringBuilder;
+    public static final String DEST = "./stamp_page_x_of_y.pdf";
+    public static final String SRC = "./fonts/tutorial/test.pdf";
+    public static final String FONT = "D:\\birch111.ttf";
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new test().manipulatePdf(DEST);
+    }
+
+
+    protected void manipulatePdf(String dest) throws Exception {
+        PdfFont font = PdfFontFactory.createFont(FONT, "Cp1251", true);
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(DEST));
+        Document doc = new Document(pdfDoc);
+        Paragraph p = new Paragraph();
+        //String sa = "Приветит2f";
+         String sa = "При жизни Серова картина экспонировалась на Таврической выставке (1905), организованной Сергеем Дягилевым, на Русской художественной выставке в Париже (1906). Кроме того, портрет демонстрировался на коллективных выставках Союза русских художников. В 1920 году картина перешла в собственность Малого театра. С 1935 года находится в Государственной Третьяковской галерее (инв. номер 28079). Там же хранится единственный известный искусствоведам эскиз к портрету, выполненный Серовым и показывающий, что ещё до начала непосредственной работы художник определил композицию, выбрал ракурс, придающий образу монументальность, и решил сделать акцент на силуэте актрисы.";
+
+        String sub = "";
+
+        int j = 1;
+        int m = 0;
+        float b = (float) 0.3;
+        int afValue = sa.indexOf('в');
+        for (int i = 0; i < sa.length(); i++) {
+            Text t = new Text("");
+
+
+            sub = sa.substring(afValue + m, afValue + j);
+            if (sa.charAt(i) == 'в') {
+                t.setText(String.valueOf(sa.charAt(i)));
+
+                //t2.setTextRise((float) 1.2);
+
+                //t2.setSkew(0,10);
+                t.setRelativePosition(0, 0, 0, 1f);
+//                  subcount = +1;
+                j++;
+                m++;
+
+
+            } else if (sa.charAt(i) == sub.charAt(0)) {
+                t.setText(String.valueOf(sa.charAt(i)));
+                //t2.setTextRise((float) 1.2);
+                //t2.setSkew(0,10);
+
+                t.setRelativePosition(0, 0, 0, (float) (1 + b));
+
+                j++;
+                m++;
+                b += 0.6;
+                System.out.println("BBB" + b);
+
+            }
+
+            t.setText(String.valueOf(sa.charAt(i)));
+
+            p.add(t);
+        }
 
 
 
-String s  = "aa2abb23";
-for(int i = 0; i<s.length();i++)
-{
-    if (Character.isAlphabetic(s.charAt(i)))
-        System.out.println( s.charAt(i)+" +");
-    else
-        System.out.println(s.charAt(i)+" -");
+        doc.add(p.setFont(font));
+        doc .close();
+    }
 }
-
-    }
-
-    }
-
